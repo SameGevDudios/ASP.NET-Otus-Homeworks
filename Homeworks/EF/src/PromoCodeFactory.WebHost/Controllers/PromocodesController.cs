d0d0703c31250c40b4f90db70a0f3249074341d4
SameGevDudios <magomed.chapaev1@gmail.com>
2025-10-01 21:11:10 +0300

    Add Promocodes/Get method

--- Homeworks/EF/src/PromoCodeFactory.WebHost/Controllers/PromocodesController.cs
+++ Homeworks/EF/src/PromoCodeFactory.WebHost/Controllers/PromocodesController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
 using PromoCodeFactory.WebHost.Models;
 
 namespace PromoCodeFactory.WebHost.Controllers
@@ -18,11 +21,29 @@ namespace PromoCodeFactory.WebHost.Controllers
         /// Получить все промокоды
         /// </summary>
         /// <returns></returns>
-        [HttpGet]
-        public Task<ActionResult<List<PromoCodeShortResponse>>> GetPromocodesAsync()
+
+        private IRepository<PromoCode> _promoCodeRepository;
+
+        public PromocodesController(IRepository<PromoCode> promoCodeRepository)
         {
-            //TODO: Получить все промокоды 
-            throw new NotImplementedException();
+            _promoCodeRepository = promoCodeRepository;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<PromoCodeShortResponse>>> GetPromocodesAsync()
+        {
+            var promoCodes = await _promoCodeRepository.GetAllAsync();
+            return promoCodes.Select(promoCode =>
+                new PromoCodeShortResponse()
+                {
+                    Id = promoCode.Id,
+                    CustomerGuid = promoCode.Customer.Id,
+                    Code = promoCode.Code,
+                    BeginDate = promoCode.BeginDate.ToString(),
+                    EndDate = promoCode.EndDate.ToString(),
+                    PartnerName = promoCode.PartnerName,
+                    ServiceInfo = promoCode.ServiceInfo
+                }).ToList();
         }
 
         /// <summary>
