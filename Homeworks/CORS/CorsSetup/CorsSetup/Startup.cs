751149fe765ddab9ecb2c5df2544fdc0a157a311
MChapaev <magomed.chapaev1@gmail.com>
2026-03-29 21:19:49 +0300

    feat(Startup): create start up class

---
+++ Homeworks/CORS/CorsSetup/CorsSetup/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace CorsSetup
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddOpenApiDocument(settings =>
+            {
+                settings.DocumentName = "v1";
+                settings.Title = "Forecast API";
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseOpenApi();
+            app.UseSwaggerUi(x =>
+            {
+                x.DocExpansion = "list";
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
