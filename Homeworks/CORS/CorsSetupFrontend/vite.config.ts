9cbb154701cb579bd9cae104bcd54d7528399ea4
MChapaev <magomed.chapaev1@gmail.com>
2026-03-30 11:37:14 +0300

    fix(vite.config): add access from localhost

--- Homeworks/CORS/CorsSetupFrontend/vite.config.ts
+++ Homeworks/CORS/CorsSetupFrontend/vite.config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+
+  return {
+    plugins: [react()],
+    server: isDev
+      ? {
+          proxy: {
+            '/api': {
+              target: 'https://localhost:5001',
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+          },
+        }
+      : undefined,
+  }
 })
