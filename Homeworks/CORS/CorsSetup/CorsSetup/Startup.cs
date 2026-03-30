08d35b51e4eb13fdcf0d81aa526f26155d93e9e7
MChapaev <magomed.chapaev1@gmail.com>
2026-03-30 12:50:08 +0300

    feat(Startup): set up CORS on backend

--- Homeworks/CORS/CorsSetup/CorsSetup/Startup.cs
+++ Homeworks/CORS/CorsSetup/CorsSetup/Startup.cs
@@ -4,8 +4,39 @@ namespace CorsSetup
 {
     public class Startup
     {
+        public readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var allowedOrigins = Configuration.GetSection("FrontendSettings:AllowedOrigins").Get<string[]>();
+
+            if (allowedOrigins is null || allowedOrigins.Count() == 0)
+            {
+                throw new Exception("No origins configured. Make sure you set tp appsettings.json");
+            }
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("FrontendPolicy", builder =>
+                {
+                    builder.WithOrigins(allowedOrigins)
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.KnownIPNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
+
             services.AddControllers();
             services.AddOpenApiDocument(settings =>
             {
@@ -37,6 +68,8 @@ namespace CorsSetup
             });
 
             app.UseRouting();
+
+            app.UseCors("FrontendPolicy");
 
             app.UseEndpoints(endpoints =>
             {
