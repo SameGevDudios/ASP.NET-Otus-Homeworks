0ad0070e3a7200b3ef36a36c764e1df9945f4281
MChapaev <magomed.chapaev1@gmail.com>
2026-03-30 22:01:56 +0300

    feat(Pcf.Administration): set up microservice

---
+++ Homeworks/RabbitMQ/src/Pcf.Administration/Pcf.Administration.Core/Services/IEmployeeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Pcf.Administration.Core.Services
+{
+    public interface IEmployeeService
+    {
+        Task UpdateAppliedPromocodesAsync(Guid id);
+    }
+}
---
+++ Homeworks/RabbitMQ/src/Pcf.Administration/Pcf.Administration.WebHost/Consumer/PromoCodeIssuedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using Pcf.Administration.Core.Services;
+using Pcf.Administration.WebHost.Messages;
+using System.Threading.Tasks;
+
+namespace Pcf.Administration.WebHost.Consumer
+{
+    public class PromoCodeIssuedConsumer : IConsumer<PromoCodeIssuedEvent>
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public PromoCodeIssuedConsumer(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public async Task Consume(ConsumeContext<PromoCodeIssuedEvent> context)
+        {
+            if (context.Message.PartnerManagerId.HasValue)
+            {
+                await _employeeService.UpdateAppliedPromocodesAsync(context.Message.PartnerManagerId.Value);
+            }
+        }
+    }
+}
